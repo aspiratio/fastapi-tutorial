0ab4f39d4333560f614cc9d043942db6ef5a3c03
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-09-19 22:58:39 +0900

    パスパラメータをお試し

--- app/main.py
+++ app/main.py
@@ -6,3 +6,10 @@ app = FastAPI()  # FastAPIのインスタンス化
 @app.get("/")  # インスタンス化したappにHTTPメソッド（オペレーションと呼ぶ）のGETで"/"のURLにアクセスがあったら下の関数を実行するという意味
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get(
+    "/user/{id}"
+)  # インスタンス化したappにHTTPメソッド（オペレーションと呼ぶ）のGETで"/"のURLにアクセスがあったら下の関数を実行するという意味
+async def get_user_id(id: int):
+    return {"item_id": id}
