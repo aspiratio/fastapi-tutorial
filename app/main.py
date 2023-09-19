659af602fb4da676887946ea0fe0335f62ab8df7
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-09-19 23:10:28 +0900

    色々な戻り値をお試し

--- app/main.py
+++ app/main.py
@@ -12,4 +12,11 @@ async def root():
     "/user/{id}"
 )  # インスタンス化したappにHTTPメソッド（オペレーションと呼ぶ）のGETで"/"のURLにアクセスがあったら下の関数を実行するという意味
 async def get_user_id(id: int):
-    return {"item_id": id}
+    return {"user_id": id, "user_name": f"user_{id}"}
+
+
+@app.get(
+    "/user_list"
+)  # インスタンス化したappにHTTPメソッド（オペレーションと呼ぶ）のGETで"/"のURLにアクセスがあったら下の関数を実行するという意味
+async def get_user_list():
+    return {"users": ["Taro", "Jiro", "Saburo"]}
