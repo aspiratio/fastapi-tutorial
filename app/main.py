5c3ca35b3c9dcfd83064c80cf035a5fbdc0532d3
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-09-20 22:12:14 +0900

    パス変換をお試し

--- app/main.py
+++ app/main.py
@@ -11,21 +11,25 @@ class ModelName(str, Enum):
 app = FastAPI()  # FastAPIのインスタンス化
 
 
+# 基本
 @app.get("/")  # インスタンス化したappにHTTPメソッド（オペレーションと呼ぶ）のGETで"/"のURLにアクセスがあったら下の関数を実行するという意味
 async def root():
     return {"message": "Hello World"}
 
 
+# パスパラメータ
 @app.get("/user/{id}")
 async def get_user_id(id: int):
     return {"user_id": id, "user_name": f"user_{id}"}
 
 
+# JSONの中にリスト
 @app.get("/user_list")
 async def get_user_list():
     return {"users": ["Taro", "Jiro", "Saburo"]}
 
 
+# Enum
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     # model_name には ModelName.alexnet, ModelName.lenet, ModelName.resnet のいずれかが入る
@@ -35,3 +39,9 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# パス変換
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
